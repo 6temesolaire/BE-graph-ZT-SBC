a35430f7a4c35967863be98dbeb114324de83e03
Kelian S <kelian.sbc@gmail.com>
2023-04-21 11:27:22 +0200

    premier test Dijkstra

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,15 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import org.insa.graphs.algorithm.AbstractSolution.Status;
+import org.insa.graphs.algorithm.utils.BinaryHeap;
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Node;
+import org.insa.graphs.model.Path;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -9,8 +19,44 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     @Override
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
-        ShortestPathSolution solution = null;
-        // TODO:
+        Graph graph = data.getGraph();
+        final int nbNodes = graph.size();
+        BinaryHeap<Label> tas = new BinaryHeap<Label>();
+        notifyOriginProcessed(data.getOrigin());
+        boolean found = false;
+        ArrayList<Label> labels = new ArrayList<Label>();
+        for (Node sommet: graph.getNodes()){
+            Label label= new Label(sommet);
+            if (label.equals(data.getOrigin())) {
+                label.setCost(0);
+                tas.insert(label);
+            }
+            labels.add(label);
+        }
+        ArrayList<Arc> predecessorArcs = new ArrayList<>();
+
+        Label min = tas.deleteMin();
+        while (min.getSommet_courant() != data.getDestination()){
+            min.setMarque(true);
+            for (Arc successor : min.getSommet_courant().getSuccessors()){
+                for (Label label : labels){
+                    if (label.getSommet_courant().equals(successor.getOrigin())){
+                        if (!label.getMarque()){
+                            double costforcomparison = label.getCost();
+                            label.setCost(Math.min(label.getCost(), min.getCost()+ successor.getLength()));
+                            if (costforcomparison!=label.getCost()){
+                                tas.insert(label);
+                                label.setPere(successor);
+                            }
+                        }
+                    }
+                }
+            }
+            min = tas.deleteMin();
+            predecessorArcs.add(min.getPere());
+        }
+        Collections.reverse(predecessorArcs);
+        ShortestPathSolution solution = new ShortestPathSolution(data, Status.OPTIMAL, new Path(graph, predecessorArcs));;
         return solution;
     }
 
--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/Label.java
@@ -3,16 +3,16 @@ package org.insa.graphs.algorithm.shortestpath;
 import org.insa.graphs.model.Arc;
 import org.insa.graphs.model.Node;
 
-public class Label {
+public class Label implements Comparable<Label> {
     private Node sommet_courant;
     private boolean marque;
-    private double cout;
+    private double coutRealise;
     private Arc pere;
 
     public Label(Node sommet_courant) {
         this.sommet_courant = sommet_courant;
         this.marque = false;
-        this.cout = Double.POSITIVE_INFINITY;
+        this.coutRealise = Double.POSITIVE_INFINITY;
         this.pere = null;
     }
 
@@ -25,7 +25,7 @@ public class Label {
     }
 
     public double getCost() {
-        return this.cout;
+        return this.coutRealise;
     }
 
     public Arc getPere() {
@@ -36,8 +36,8 @@ public class Label {
         this.marque = marque;
     }
 
-    public void setCout(double cout) {
-        this.cout = cout;
+    public void setCost(double cout) {
+        this.coutRealise = cout;
     }
 
     public void setPere(Arc pere) {
@@ -45,6 +45,6 @@ public class Label {
     }
 
     public int compareTo(Label other) {
-        return Double.compare(this.cout, other.cout);
+        return Double.compare(this.coutRealise, other.coutRealise);
     }
 }
